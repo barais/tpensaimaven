5c93da3bdbf41c0bc4bfcb62a937e86e19f45ffe
Matthew McCullough <matthew@github.com>
2014-09-16 15:36:50 +0200

    new file

---
+++ src/main/java/fr/ensai/tpmaventp/App.java
@@ -0,0 +1,16 @@
+package fr.ensai.tpmaventp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		try {
+			System.out.println("Hello World!");
+		} catch (Exception e) {
+		}
+
+	}
+}
